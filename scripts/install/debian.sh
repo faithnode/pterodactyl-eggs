e1e99c9fbaae960cb469b5b93ec35da9d3bed31e
Roman Glushinskiy <mail@poma4ka.me>
2026-07-08 10:13:49 +0300

    add global timeout

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -5,6 +5,7 @@ set -eo pipefail;
 mkdir -p /mnt/server/ && cd /mnt/server/;
 apt update && apt install -y curl jq;
 
+# === HELPERS ===
 function fatal() {
   echo "$@";
   exit 1;
@@ -14,7 +15,19 @@ function curl() {
   /usr/bin/curl -sf -L --show-error "$@";
 }
 
+# === GLOBAL TIMEOUT ===
+MAIN_PID=$$
+(
+  sleep 600
+  echo "Global timeout exeeded..."
+  kill -9 $MAIN_PID 2>/dev/null
+) &
+TIMER_PID=$!
+
+# === EXIT TRAP ===
 function before_exit() {
+  kill $TIMER_PID 2>/dev/null
+
   if [ $? == 0 ]; then
       echo -e "Install complete"
   else
