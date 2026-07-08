865d8237d5c2dc061680fcab303c4e206b26e7a9
Roman Glushinskiy <mail@poma4ka.me>
2026-07-08 10:43:06 +0300

    fix paper projects

--- eggs/minecraft-proxy/waterfall/install.sh
+++ eggs/minecraft-proxy/waterfall/install.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-PAPER_API_URL='https://api.papermc.io'
+PAPER_API_URL='https://fill.papermc.io'
 PROJECT=waterfall
 VERSION=$_INSTALL_WATERFALL_VERSION
 
 echo -e "Getting $PROJECT versions";
-VERSIONS="$(curl "$PAPER_API_URL/v2/projects/$PROJECT" | jq '.versions')"
+VERSIONS="$(curl "$PAPER_API_URL/v3/projects/$PROJECT" | jq -r '.versions | to_entries | map(.value) | flatten')"
 
 if [[ -z $VERSION || ${VERSION,,} == "latest" ]]; then
   echo -e "Getting latest version";
-  VERSION=$(echo "$VERSIONS" | jq -r '.[-1]');
+  VERSION=$(echo "$VERSIONS" | jq -r '.[0]');
 else
   if [[ -z $(echo "$VERSIONS" | jq -r "$(printf '.[] | select(. == "%s")' "$VERSION")") ]]; then
     fatal "Version $VERSION not found";
   fi
 fi
 
-echo -e "Getting $PROJECT latest build"
-LATEST_BUILD=$(curl "$PAPER_API_URL/v2/projects/$PROJECT/versions/$VERSION" | jq -r '.builds[-1]')
 
-echo -e "Getting download link"
-JARFILE="$PROJECT-$VERSION-$LATEST_BUILD.jar"
+echo -e "Getting $PROJECT latest build data"
+BUILD_DATA="$(curl "$PAPER_API_URL/v3/projects/$PROJECT/versions/$VERSION/builds/latest" | jq -r '.downloads."server:default"')"
+
+JARFILE="$(echo "$BUILD_DATA" | jq -r ".name")"
+DOWNLOAD_URL="$(echo "$BUILD_DATA" | jq -r ".url")"
 
 echo -e "Removing old jars";
 rm -rf ./$PROJECT*.jar;
 
 echo -e "Downloading $JARFILE"
-curl "$PAPER_API_URL/v2/projects/$PROJECT/versions/$VERSION/builds/$LATEST_BUILD/downloads/$JARFILE" -o "$JARFILE"
+curl "$DOWNLOAD_URL" -o "$JARFILE"
 
 echo -e "Agree eula";
 echo "eula=true" > "eula.txt";
